9b1317a80c72fcb960e450f4b92493566543cbfd
miftanurfarid <miftanurfarid@gmail.com>
2017-12-28 10:44:17 +0700

    fix m-files

---
+++ Show2DImage.m
@@ -0,0 +1,34 @@
+%Jason T. Parker
+
+function ...
+showduadimensi(data,bounds,f_hand,title_string,cscale,x_string,y_string)
+%This function plots a normalized image from 2d data produced by ...pcmf image
+
+%data is a 2d matrix of raw (i.e. complex) pixel values
+
+%bounds is the 1 by 6 vector of axis limits x,y,z ordering
+
+%fignum is the desired figure number
+
+%title string is the title string of the figure
+
+%cscale is the desired color scale
+
+if nargin < 6
+  x_string = 'X (m)';
+  y_string = 'Y (m)';
+end
+
+figure(f_hand)
+clf
+imagesc(bounds(1,1:2),bounds(1,3:4),20*log10(abs(data/max(abs(data(:))))),cscale);
+
+%imagesc(bounds(1,1:2),bounds(1,3:4),20*log10(abs(data)),cscale);
+set(gca,'YDir','normal','fontsize',11,'fontweight','bold');
+grid off
+axis square
+xlabel(x_string,'fontsize',12,'fontweight','bold');
+ylabel(y_string,'fontsize',12,'fontweight','bold');
+title(title_string,'fontsize',12,'fontweight','bold');
+colormap('jet');
+colorbar
---
+++ ShowImages.m
@@ -0,0 +1,34 @@
+%Jason T. Parker
+
+function ShowImages(result,bounds,spacing,f_hand,title_string,cscale)
+
+if bounds(5) == bounds(6) %2-D image in X-Y plane
+    Show2DImage(result,bounds,f_hand,title_string,cscale,'X (m)','Y (m)');
+elseif bounds(1) == bounds(2) %2-D image in Y-Z plane
+    %Fix the bounds
+    bounds = bounds(1,3:end);
+    Show2DImage(squeeze(result).',bounds,f_hand,title_string,cscale,'Y (m)','Z (m)');
+elseif bounds(3) == bounds(4) %2-D image in X-Z plane
+    %Fix the bounds
+    bounds = [bounds(1,1:2) bounds(1,5:6)];
+    Show2DImage(squeeze(result).',bounds,f_hand,title_string,cscale,'X (m)','Z (m)');
+else
+    figure(f_hand)
+    clf
+
+    %Generate the coordinate vectors
+    x = bounds(1):spacing:bounds(2);
+    y = bounds(3):spacing:bounds(4);
+    z = bounds(5):spacing:bounds(6);
+
+    % %Call plotting code option 1
+    % contours = linspace(cscale(1),cscale(2),20);
+    % plot3 patch(f hand,x,y,z,20*log10(abs(result/max(abs(result(:))))),contours,linsp
+
+    %Plotting code, option 2
+    isosurface(x,y,z,20*log10(abs(result)/max(abs(result(:)))),cscale(1));
+    axis(bounds);
+    title(title_string);
+end
+
+end
